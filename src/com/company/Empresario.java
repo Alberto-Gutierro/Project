25c5ff839b7772d6ed03cb6395bd8fd6ecee189c
Ivan Salinas <isalinasp90@elpuig.xeill.net>
2018-03-14 19:13:42 +0100

    Menu principal, Menu Empresario + Corrección de errores

--- src/com/company/Empresario.java
+++ src/com/company/Empresario.java
@@ -3,22 +3,27 @@ package com.company;
 import java.util.Scanner;
 
 public class Empresario {
+
+
     void menuEmp(){
 
+
         Scanner scanner = new Scanner(System.in);
+        FICHA ficha=new FICHA();
+
         int opcion;
 
         do {
             System.out.println("    ------------------------------");
             System.out.println("    Bienvenido al Menu De Empresario" );
             System.out.println("    ¿Que Deseas Hacer?");
-            System.out.println("    1. Abrir Negocio\n    2 Horarios \n    3 Normativa Locales\n \n\n    0 Salir");
+            System.out.println("    1. Abrir Locl\n    2. Horarios \n \n\n    0 Salir");
             System.out.println("    ------------------------------");
             opcion = scanner.nextInt();
             scanner.nextLine();
             switch (opcion) {
-                case '1':
-                    new FICHA();
+                case 1:
+
                     break;
 
                 default:
