506ee5ceb31a02970d1cc703071292048630bd14
ericvillan <evillanueva@elpuig.xeill.net>
2018-04-09 19:05:55 +0200

    c

--- src/com/company/view/MenuAdmin.java
+++ src/com/company/view/MenuAdmin.java
@@ -1,11 +1,17 @@
 package com.company.view;
+import com.company.view.MostrarDatos;
+import com.company.model.Empresario;
+import java.util.Scanner;
+
 
 public class MenuAdmin {
 
     public void mostrar() {
-
-
-
+    MostrarDatos mostrarDatos =  new MostrarDatos();
+        MostrarDatos.nombreEmpresario;
+        MostrarDatos.apellidoEmpresario;
+        MostrarDatos.nickEmpresario;
+        MostrarDatos.NIF = scanner.nextInt();
     }
 
 }
