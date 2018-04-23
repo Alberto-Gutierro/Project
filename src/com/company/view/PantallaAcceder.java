36c078cd28c51768f479cd28265a79cba02d81aa
ericvillan <evillanueva@elpuig.xeill.net>
2018-04-23 17:42:00 +0200

    c

--- src/com/company/view/PantallaAcceder.java
+++ src/com/company/view/PantallaAcceder.java
@@ -1,49 +1,25 @@
 package com.company.view;
 
 import com.company.managers.ManagerCentroComercial;
+import com.company.model.Empresario;
+import com.company.view.widget.EditText;
+
 
 import java.util.Scanner;
 
-public class PantallaAcceder{
+public class PantallaAcceder {
 
-    public void mostrar(ManagerCentroComercial managerCentroComercial) {
-        Scanner scanner = new Scanner(System.in);
+    public Empresario empresario = new Empresario();
+    public Scanner scanner = new Scanner(System.in);
 
-        int opcion;
+    public void PantallaAcceso(ManagerCentroComercial managerCentroComercial) {
 
-        do {
-            System.out.println("    ------------------------------");
-            System.out.println("    Bienvenido a la Pantalla de Acceso de Administrador");
-            System.out.println("    ¿Que tipo de datos quiere?");
-            System.out.println("    1. Tiendas\n    2 Mapa \n    3 Empresarios\n \n\n    0 Salir");
-            System.out.println("    ------------------------------");
-            opcion = scanner.nextInt();
-            scanner.nextLine();
+        System.out.println("DATOS DEL EMPRESARIO");
+        EditText editText = new EditText();
 
-            switch (opcion) {
-                case 1:
-                    MostrarDatos mostrarDatos = new MostrarDatos();
-                    mostrarDatos.mostrarFichaTienda();
-                    break;
-                case 2:
-                    MenuPrincipal menuPrincipal=new MenuPrincipal();
-                    menuPrincipal.mostrar(managerCentroComercial);
-                    break;
-                case 3:
-                    MostrarDatos mostrarDatos2 = new MostrarDatos();
-                    mostrarDatos2.mostrarFichaEmpresario();
-                    break;
-                case 0:
-                    break;
-
-                default:
-                    System.out.println("OPCIÓN INCORRECTA!");
-
-                    break;
-            }
-
-        } while (opcion != 0);
-
+        empresario.nombreEmpresario = editText.read("Introduzca su nombre:\n");
+        empresario.NIF = editText.read("Introduzca un dni:\n");
+        managerCentroComercial.añadirEmpresario(empresario.nombreEmpresario, empresario.apellidoEmpresario, empresario.nickEmpresario, empresario.NIF, empresario.telEmpresario);
     }
 
 }
